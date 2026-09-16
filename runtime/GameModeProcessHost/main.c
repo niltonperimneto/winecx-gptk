d55d612c42d03bd6627cc6e2148a05633e029f74
Nilton Perim Neto <niltonperimneto@gmail.com>
2026-09-15 23:27:06 -0300

    runtime: package Game Mode process host

---
+++ runtime/GameModeProcessHost/main.c
@@ -0,0 +1,38 @@
+/* Same-PID trampoline that gives a matched Wine game a Game Mode app context. */
+
+#include <errno.h>
+#include <spawn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <crt_externs.h>
+
+#ifndef _POSIX_SPAWN_DISABLE_ASLR
+#define _POSIX_SPAWN_DISABLE_ASLR 0x0100
+#endif
+
+int main(int argc, char **argv)
+{
+    posix_spawnattr_t attributes;
+    int error;
+
+    if (argc < 2 || !argv[1] || !*argv[1])
+    {
+        fputs("WHISKY_CHILD_POLICY result=host-failed error=missing-loader\n", stderr);
+        return 64;
+    }
+
+    unsetenv("WHISKY_GAME_MODE_REQUESTED");
+    error = posix_spawnattr_init(&attributes);
+    if (!error)
+    {
+        error = posix_spawnattr_setflags(&attributes, POSIX_SPAWN_SETEXEC | _POSIX_SPAWN_DISABLE_ASLR);
+        if (!error) error = posix_spawn(NULL, argv[1], NULL, &attributes, argv + 1, *_NSGetEnviron());
+        posix_spawnattr_destroy(&attributes);
+    }
+
+    fprintf(stderr, "WHISKY_CHILD_POLICY result=host-failed error=%d\n", error ?: errno);
+    execv(argv[1], argv + 1);
+    return 126;
+}
